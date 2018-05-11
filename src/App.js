e5923a79d8606f2b25aa1521fafd013397dbd00d
Nery Alexandra Campusano Vargas <n.campusanov@gmail.com>
2018-05-11 19:31:00 -0400

    psudo selectors and media queries

--- src/App.js
+++ src/App.js
@@ -1,52 +1,102 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+// import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
     persons : [
-      { name: 'Nery', age: 21},
-      { name: 'Pedro', age: 22},
-      { name: 'Nancy', age: 49}
-    ]
+      { id: 1, name: 'Nery', age: 21},
+      { id: 2,name: 'Pedro', age: 22},
+      { id: 3,name: 'Nancy', age: 49}
+    ],
+    showPersons : false
+  }
+  nameChangedHandler = (event, id) =>{
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons : persons});
   }
 
-  switchNameHandler = (newName) =>{
-   //Dont do this-> this.state.persons[0].name = "Alexandra";
-   this.setState({persons : [
-    { name: newName, age: 21},
-    { name: 'Pedro', age: 22},
-    { name: 'Nancy', age: 49}
-  ]})
+  deletePersonHandler = (index) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) =>{
-    this.setState({persons : [
-      { name: 'Alexandra', age: 21},
-      { name: event.target.value, age: 22},
-      { name: 'Nancy', age: 49}
-    ]})
+  togglePersonsHndler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
   }
-
   render() {
-    //Using the nonymous function to pass values to a function, it's not recommended, React can render the page a little too often. So the best practice is using bind.
+    //in-line styles
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'//,
+      // ':hover' : {
+      //   backgroundColor : 'lightgreen',
+      //   color: 'black'
+      // }
+    }
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          { this.state.persons.map((person, index) => {
+
+            return <Person
+            click={() => this.deletePersonHandler(index)}
+            name = {person.name}
+            age = {person.age}
+            key = {person.id}
+            changed = {(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}          
+          </div>
+      );
+
+      style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor : 'salmon',
+      //   color : 'black'
+      // }
+    }
+
+    const classes = [];
+
+    if(this.state.persons.length <= 2)
+      classes.push('red');
+    
+    if(this.state.persons.length <= 1)
+      classes.push('bold');
+    //Using the nonymous function to pass values to a function (() => this.togglePersonsHndler), it's not recommended, React can render the page a little too often. So the best practice is using bind. 
     return (
-      <div className="App">
-        <h1>Hi, I'm a react app</h1>
-        <p>This is working</p>        
-        <button onClick={() => this.switchNameHandler('Alexandra')}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} />
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Nery')}
-        changed={this.nameChangedHandler}>My hobbies: Love my girlfriend and be awesome.</Person>
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+      // <StyleRoot>
+          <div className="App">
+          <h1>Hi, I'm a react app</h1>
+          <p className={classes.join(' ')}>This is working</p>        
+          <button
+          style={style}
+          onClick={this.togglePersonsHndler}>Toggle Persons</button>       
+          {persons}      
       </div>
+    /* </StyleRoot> */
 
     );
     //Equivalent of the code above.
@@ -54,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 
+import './Person.css'
+// import Radium from 'radium';
+
 const person = (props) => {
+    // const style = {
+    //     '@media (min-width: 500px)' :{
+    //         width: '450px'
+    //     }
+    // }
     return (
-        <div>
+        <div className="Person" >
             <p onClick={props.click}>I'm a person, my name is {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -12,3 +20,4 @@ const person = (props) => {
 }
 
 export default person;
+// export default Radium(person);
